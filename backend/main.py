d06f67dfb37fdde5d43b084d55456d0ebe833c94
Piotr Gazda <pjgazda@interia.pl>
2021-09-11 23:51:42 +0200

    Backend adjusted to process POST

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,8 @@
 
 from flask import Flask, jsonify, request, make_response
 from flask_cors import CORS
-from Detection import detect
+import json
+#from Detection import detect
 # ... other import statements ...
 
 # creating the Flask application
@@ -13,13 +14,34 @@ CORS(app)
 
 @app.route('/predictions')
 def get_predictions():
+    '''
     predictions = detect()
     response = make_response(str(predictions), 200)
     response.mimetype = "text/plain"
     print(response)
     return response
+    '''
+    print("I am GET")
+    return "I am working"
 
-@app.route('/statistics', methods=['POST'])
-def add_exam():
-
-    return "Viewing statistics", 201
+@app.route('/setup', methods=['POST'])
+def setup_detection():
+    print("I am POST")
+    print("Request body: ")
+    req = request.get_json()
+    print(req)
+    print("Request part: ")
+    print(req['endDay'])
+    response_body = {
+      "startDay": "Wed",
+      "endDay": "Thurs",
+      "startTime": "12:34",
+      "endTime": "15:20",
+      "totalDetections": []
+    }
+    response_body_json = json.dumps(response_body)
+    response = make_response(response_body_json, 200)
+    response.mimetype = "application/json"
+    print('Response: ')
+    print(response)
+    return response
